b6ea9d9b35f36eeada01231681c99135c96dc6b6
lxxs13 <l18111952@itcj.edu.mx>
2022-11-21 20:17:18 -0700

    Validar ususuario logeado

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,16 @@
-import { AfterContentInit, Component } from '@angular/core';
+import { AfterContentInit, Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements AfterContentInit {
+export class DashboardComponent implements OnInit, AfterContentInit {
   optionSelected: string = 'Dashboard';
   visible: boolean = true;
 
@@ -19,7 +20,14 @@ export class DashboardComponent implements AfterContentInit {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver, private _router: Router) {}
+  constructor(private breakpointObserver: BreakpointObserver, private _router: Router, private _auth: AuthService) {}
+  
+  ngOnInit(): void {
+    debugger
+    if(this._auth.users.length === 1){
+      this._router.navigate(['/']);
+    }
+  }
 
   ngAfterContentInit(): void {
     // throw new Error('Method not implemented.');
